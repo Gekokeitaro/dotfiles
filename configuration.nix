5f6550d19eb5634230f3bf1adae3bba6cbae9a8c
pbugarin <pablo.bugarin.camara@gmail.com>
2026-01-03 16:46:50 +0100

    first commit

---
+++ configuration.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixmox"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Madrid";
+
+  # Layout del teclado en español para la terminal
+  console.keyMap = "es";
+
+  services.xserver = {
+    enable = true;
+    xkb.layout = "es"; # Layout del teclado en español para UI
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.qtile.enable = true;
+  };
+
+  services.displayManager.ly.enable = true;
+	
+  users.users.nixmox= {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  # Configuración SSH
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false; # True la primera vez para añadir las claves SSH
+      AllowUsers = [ "nixmox" ];
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
+
