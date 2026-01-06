441fb95c1e7d5a74b7a7422534aa5197ab330ee9
PBugarin <pablo.bugarin.camara@gmail.com>
2026-01-06 23:40:12 +0100

    FEATURE - Added some Ghostty  config

--- user-modules/ghostty.nix
+++ user-modules/ghostty.nix
@@ -5,6 +5,11 @@
     settings = {
       font-size = 18;
       theme = "Aurora";
+      window-decoration = false;
+
+      # Transaparency
+      background-opacity = 0.8;
+      background-blur-radius = 20;
     };
   };
 }
