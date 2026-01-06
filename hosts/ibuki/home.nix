41c068c31d5468a154fb20c986a29f6e0a95c902
pbugarin <pablo.bugarin.camara@gmail.com>
2026-01-06 12:41:15 +0100

    added ibuki host

---
+++ hosts/ibuki/home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ibuki";
+  home.homeDirectory = "/home/ibuki";
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [ ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+}
