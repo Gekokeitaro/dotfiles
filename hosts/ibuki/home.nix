f0937cd456dfa988b18986823ebf66459beeb2aa
PBugarin <pablo.bugarin.camara@gmail.com>
2026-01-11 21:20:06 +0100

    FEATURE - Alacritty re-enable. Changed old wrong niri config

--- hosts/ibuki/home.nix
+++ hosts/ibuki/home.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [ 
-    #./../modules/alacritty
-    ../../modules/foot
+    ../../modules/alacritty
+    #./../modules/foot
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
