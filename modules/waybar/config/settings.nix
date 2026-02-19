d1fe9b74e258714921b07e9ca54f7dc31f516037
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-19 09:42:46 +0100

    REFACTOR - Style bars as background image

--- modules/waybar/config/settings.nix
+++ modules/waybar/config/settings.nix
@@ -8,10 +8,6 @@
       output = [ "eDP-1" ];
 
       modules-left= [
-        "niri/workspaces"
-      ];
-
-      modules-center= [
         "battery"
         "cpu"
         "memory"
@@ -30,74 +26,121 @@
       };
 
       "battery" = {
-        format = "<span foreground='#FFF1E8'>BAT\t<sup>{capacity}/100%</sup></span>";
+        format = "<span size='large' foreground='#FFF1E8'>BAT <span rise='5pt' size='small'>{capacity}/100</span></span>";
 	interval = 15;
         states = {
+          "95" = 95;
           "90" = 90;
-          "82" = 82;
+          "85" = 85;
+          "80" = 80;
           "75" = 75;
-          "62" = 62;
+          "70" = 70;
+          "65" = 65;
+          "60" = 60;
+          "55" = 55;
           "50" = 50;
-          "44" = 44;
-          "37" = 37;
+          "45" = 45;
+          "40" = 40;
+          "35" = 35;
+          "30" = 30;
           "25" = 25;
-          "17" = 17;
+          "20" = 20;
+          "15" = 15;
           "10" = 10;
+          "5" = 5;
+          "0" = 0;
         };
       };
 
       "cpu" = {
-        format = "<span foreground='#FFF1E8'>CPU\t<sup>{usage}/100%</sup></span>";
+        format = "<span size='large' foreground='#FFF1E8'>CPU <span rise='5pt' size='small'>{usage}/100</span></span>";
         interval = 10;
 	states = {
+          "95" = 95;
           "90" = 90;
-          "82" = 82;
+          "85" = 85;
+          "80" = 80;
           "75" = 75;
-          "62" = 62;
+          "70" = 70;
+          "65" = 65;
+          "60" = 60;
+          "55" = 55;
           "50" = 50;
-          "44" = 44;
-          "37" = 37;
+          "45" = 45;
+          "40" = 40;
+          "35" = 35;
+          "30" = 30;
           "25" = 25;
-          "17" = 17;
+          "20" = 20;
+          "15" = 15;
           "10" = 10;
+          "5" = 5;
+          "0" = 0;
         };
       };
 
       "memory" = {
-        format = "<span fgcolor='#FFF1E8'>RAM\t<sup>{percentage}/100%</sup></span>";
+        format = "<span size='large' fgcolor='#FFF1E8'>RAM <span rise='5pt' size='small'>{percentage}/100</span></span>";
         interval = 10;
         states = {
+          "95" = 95;
           "90" = 90;
-          "82" = 82;
+          "85" = 85;
+          "80" = 80;
           "75" = 75;
-          "62" = 62;
+          "70" = 70;
+          "65" = 65;
+          "60" = 60;
+          "55" = 55;
           "50" = 50;
-          "44" = 44;
-          "37" = 37;
+          "45" = 45;
+          "40" = 40;
+          "35" = 35;
+          "30" = 30;
           "25" = 25;
-          "17" = 17;
+          "20" = 20;
+          "15" = 15;
           "10" = 10;
+          "5" = 5;
+          "0" = 0;
         };
       };
 
       "disk" = {
-         format = "ENC\t<sup>{percentage_used}/100%</sup>";
-         interval = 60;
-       };
+        format = "<span size='large' fgcolor='#FFF1E8'>ENC <span rise='5pt' size='small'>{percentage_used}/100</span></span>";
+        interval = 60;
+        states = {
+          "95" = 95;
+          "90" = 90;
+          "85" = 85;
+          "80" = 80;
+          "75" = 75;
+          "70" = 70;
+          "65" = 65;
+          "60" = 60;
+          "55" = 55;
+          "50" = 50;
+          "45" = 45;
+          "40" = 40;
+          "35" = 35;
+          "30" = 30;
+          "25" = 25;
+          "20" = 20;
+          "15" = 15;
+          "10" = 10;
+          "5" = 5;
+          "0" = 0;
+        };
+      };
 
       "temperature" = {
-        format = "TMP\t<sup>{temperatureC}</sup>";
+        format = "<span size='large'>TMP {temperatureC}º</span>";
         interval = 15;
       };
 
-      "niri/workspaces" = {
-        format = "{name}";
-        all-outputs = false;
-        persistent-workspaces = {
-          "1" = [];
-          "2" = [];
-          "3" = [];
-        };
+      "clock" = {
+        format = "<span size='large'></span>";
+        interval = 60;
       };
     };
   };
