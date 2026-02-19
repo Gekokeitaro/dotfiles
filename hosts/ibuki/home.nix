1ad4c803b4893259be044955b0caf7201631715f
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-19 10:17:58 +0100

    FEATURE - Added Stone Soup

--- hosts/ibuki/home.nix
+++ hosts/ibuki/home.nix
@@ -16,7 +16,10 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [ ];
+  home.packages = with pkgs; [
+    crawl
+    crawlTiles
+  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
