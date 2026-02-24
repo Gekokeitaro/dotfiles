556ffbd7ab3aae0612f0510f59907f8e38b22158
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-24 12:52:22 +0100

    FEATURE - Opencode using module system

--- modules/opencode/default.nix
+++ modules/opencode/default.nix
@@ -1,9 +1,16 @@
-{ pkgs, ... }:
-{
-  programs.opencode = {
-    enable = true;
-    settings = {
-      default_agent = "plan";
-    };
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.homeModules.opencode;
+in {
+  options.homeModules.opencode = {
+    enable = mkEnableOption "enable opencode module";
+  };
+
+  config = mkIf cfg.enable {
+    programs.opencode.enable = true;
+    #homeModules.opencode.settings.default_agent = "plan";
   };
 }
