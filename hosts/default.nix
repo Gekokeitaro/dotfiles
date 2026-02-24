17784bee378c35304c124d40bd64106d5bbe541b
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-24 11:09:32 +0100

    FIX - Forgot to rewrite path

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 let
   lib = inputs.nixpkgs.lib;
-  hostsDirs = import ../lib/getDirs.nix { inherit lib; path=../hosts; };
+  hostsDirs = import ../utils/getDirs.nix { inherit lib; path=../hosts; };
 in
 builtins.listToAttrs (
   map ( host: {
