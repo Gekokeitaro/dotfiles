de4418b1197cd7218833dbf945310af71b0ddb6f
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-24 20:47:29 +0100

    FEATURE - Alacritty now use module system

--- hosts/ibuki/home.nix
+++ hosts/ibuki/home.nix
@@ -22,6 +22,7 @@
   ];
 
   homeModules.opencode.enable = true;
+  homeModules.alacritty.enable = true;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
