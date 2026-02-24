9ebd7f317cb3f5a97166342afc42b06ca8b72b38
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-24 21:45:49 +0100

    FEATURE - Switching to full auto import modules

--- hosts/ibuki/home.nix
+++ hosts/ibuki/home.nix
@@ -2,11 +2,8 @@
 
 {
   imports = [ 
-    ../../modules/alacritty
+    ../../modules
     #./../modules/foot
-    ../../modules/waybar
-    ../../modules/opencode
-    ../../modules/lazygit
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
