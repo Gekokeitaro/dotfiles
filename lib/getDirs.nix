5fb573d34ad9326630e8e29f31b756dea6bb094e
PBugarin <pablo.bugarin.camara@gmail.com>
2026-02-24 11:05:17 +0100

    Chore - delete lib due to rename

--- lib/getDirs.nix
+++
@@ -1,7 +0,0 @@
-{ lib, path }:
-
-builtins.filter (x: x != null) (
-  lib.mapAttrsToList 
-  (name: value: if value == "directory" then name else null) 
-  ( builtins.readDir path )
-)
--- lib/getHosts.nix
+++
@@ -1,9 +0,0 @@
-{ lib }:
-let
-  hostsDir = ../hosts;
-in
-builtins.filter (x: x != null) (
-  lib.mapAttrsToList 
-  (name: value: if value == "directory" then name else null) 
-  ( builtins.readDir hostsDir )
-)
