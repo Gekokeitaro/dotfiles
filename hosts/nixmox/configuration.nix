04ad51737085bf6fe29d0e710e9090460c6ff00e
pbugarin <pablo.bugarin.camara@gmail.com>
2026-03-02 21:48:51 +0100

    FEATURE - NVF enabled on nixmox

--- hosts/nixmox/configuration.nix
+++ hosts/nixmox/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../root-modules/podman
+      ../../root-modules
     ];
 
   boot.loader.systemd-boot.enable = true;
