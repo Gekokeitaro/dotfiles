f062cbe941b6c1ed349432de1a87493696267f53
PBugarin <pablo.bugarin.camara@gmail.com>
2026-03-14 21:24:52 +0100

    FEATURE - Added working config to connect to koboldCPP

--- modules/opencode/default.nix
+++ modules/opencode/default.nix
@@ -11,6 +11,22 @@ in {
 
   config = mkIf cfg.enable {
     programs.opencode.enable = true;
-    programs.opencode.settings.default_agent = "plan";
+    programs.opencode.settings = {
+      default_agent = "plan";
+      provider = {
+        koboldcpp = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "KoboldCPP";
+          options = {
+            baseURL = "http://192.168.18.3:5001/v1";
+          };
+          models = {
+            "koboldcpp/qwen2.5-coder-14b-instruct-q4_k_m" = {
+              name = "Qwen 2.5 Coder 14B";
+            };
+          };
+        };
+      };
+    };
   };
 }
