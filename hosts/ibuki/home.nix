1ccd549e7b0f6761ab9e28cec1a996c0183a1ea2
PBugarin <pablo.bugarin.camara@gmail.com>
2026-03-15 22:07:01 +0100

    FEATURE - Ghostty as default term + quit windows title

--- hosts/ibuki/home.nix
+++ hosts/ibuki/home.nix
@@ -20,12 +20,15 @@
   ];
 
   homeModules.opencode.enable = true;
-  homeModules.alacritty.enable = true;
+  #homeModules.alacritty.enable = true;
   homeModules.waybar.enable = true;
   homeModules.lazygit.enable = true;
   #homeModules.neovim.enable = true;
   homeModules.nvf.enable = true;
+  homeModules.ghostty.enable = true;
 
+  xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
