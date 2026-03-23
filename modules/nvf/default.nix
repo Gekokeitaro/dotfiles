ef8c284e921d517fba16f2aa2feb592001741589
PBugarin <pablo.bugarin.camara@gmail.com>
2026-03-23 18:47:52 +0100

    FEATURE - Implemented bubbles theme to lualine

--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -40,7 +40,30 @@ in {
 
         statusline.lualine = {
           enable = true;
-          theme = "horizon";
+          
+          componentSeparator = {};
+          sectionSeparator = { 
+            left = ""; 
+            right = ""; 
+          };
+
+          activeSection = {
+            a = [ '' {"mode", separator = { left = "" }, right_padding = 2 } '' ];
+            b = [ '' "filename", "branch" '' ];
+            c = [];
+            x = [];
+            y = [ '' "filetype", "progress" ''];
+            z = [ '' { "location", separator = { right = "" }, left_padding = 2 } '' ];
+          };
+
+          inactiveSection = {
+            a = [ '' "filename" '' ];
+            b = [];
+            c = [];
+            x = [];
+            y = [];
+            z = [ '' "location" '' ];
+          };
         };
 
         telescope.enable = true;
