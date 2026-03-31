b8e3ce2d9cfc5838804d6c03127640d3ae6e1469
PBugarin <pablo.bugarin.camara@gmail.com>
2026-03-31 20:31:11 +0200

    FEATURE - Autocmd to update modified field in .md yaml

--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -37,6 +37,35 @@ in {
           };
         };
 
+        autocmds = [
+          {
+            desc = "Updates modified field in markdown YAML";
+            pattern = [ "*.md" ];
+            event = [ "BufWritePre" ];
+            callback = lib.generators.mkLuaInline ''
+              function(args)
+                local bufnr = args.buf
+                local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 50, false)
+                if lines[0] == "---" then
+                  for i = 1, #lines do
+                    if lines[i] == "---" then 
+                      break
+                    end
+                    
+                    if lines[i]:match("^modified:") then
+                      local prefix = lines[i]:match("^(.-):")
+                      local updated_time = os.date("%Y-%m-%dT%H:%M:%SZ")
+                      vim.api.nvim_buf_set_lines(bufnr, i-1, i, false, { prefix .. ": " .. updated_time})
+                      break
+                    end
+                  end
+                end
+              end
+            '';
+          }
+        ];
+
+
         diagnostics = {
           enable = true;
           nvim-lint = {
